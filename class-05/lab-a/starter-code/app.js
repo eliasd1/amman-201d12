8943d5d6378eacdd0b8604534c180b5f4d742098
eliasd1 <elidadoush23@gmail.com>
2020-12-31 12:47:03 +0200

    added sum function

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -9,12 +9,7 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
-    var total = a+b;
-    var returnArray = [
-        total,
-        'The sum of 4 and 7 is 11.'
-    ]
-    return returnArray;
+    return [a+b, `The sum of ${a} and ${b} is ${a+b}.`]
 }
 
 // Here is the test for sum(); uncomment it to run it
@@ -32,19 +27,11 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-    var total =  a*b;
-
-    var returnArray = [
-        total,
-        "The product of 5 and 9 is 45."
-    ];
-
-    return returnArray;
-
+  
 }
 
 // Here is the test for multiply(); uncomment it to run it
-testMultiply(5,9);
+// testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
