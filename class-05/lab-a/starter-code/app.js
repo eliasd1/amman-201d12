780bf06c38979b20664c9b11008bfc27357ca6b7
eliasd1 <elidadoush23@gmail.com>
2020-12-31 13:16:57 +0200

    fixed code

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -70,7 +70,11 @@ Test this function by hand in the console to get it working, and when you think 
 var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-    return [sumArr.reduce((acc,next) => sum(acc,next) , 0), "2,3,4 was passed in as an array of numbers, and 9 is their sum."]
+    var total = 0;
+    for(let i = 0; i<sumArr.length; i++){
+        total = sum(total, sumArr[i])[0]
+    }
+    return [total, "2,3,4 was passed in as an array of numbers, and 9 is their sum."]
 }
 
 // Here is the test for sumArray(); uncomment it to run it
